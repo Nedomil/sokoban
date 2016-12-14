26a2bce5bd341716a9f38d222ad771d56bc3f33f
Nedomil <nedomil@gmail.com>
2016-12-14 19:41:48 +0100

    Delete all.

--- Sokoban/Assets/Scripts/Exceptions/InvalidMoveException.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class InvalidMoveException : System.Exception {
-
-	public InvalidMoveException() {
-		Debug.Log ("Unauthorized move. Please use a valid direction.");
-	}
-}
--- Sokoban/Assets/Scripts/Exceptions/UnknownSymbolException.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class UnknownSymbolException : System.Exception {
-
-	public UnknownSymbolException() {
-		Debug.Log ("Level file contains unknown symbols. Fix it ore change it to a valid Level");
-	}
-}
