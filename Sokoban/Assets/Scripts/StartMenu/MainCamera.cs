a8248d0b8f33f04499ea0997c591b25bf33197b8
Nedomil <nedomil@gmail.com>
2016-12-18 17:52:47 +0100

    Addes simple start menu.

---
+++ Sokoban/Assets/Scripts/StartMenu/MainCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+	public Camera cam;
+	public static readonly float ROTATION_SPEED = 0.005f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cam.transform.Rotate (0, -ROTATION_SPEED, 0);		
+	}
+}
---
+++ Sokoban/Assets/Scripts/StartMenu/StartButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void startGame() {
+		Application.LoadLevel (1);
+	}
+}
